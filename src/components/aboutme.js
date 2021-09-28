b6212df34a3bf09e584419fec316bd8a60103521
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-28 19:50:23 -0400

    created aboutme component

---
+++ src/components/aboutme.js
@@ -0,0 +1,28 @@
+const AboutMe = () => {
+    return (
+        <div class="jumbotron">
+      <div class="container text-center">
+        <h1>Zana Mathuthu's Professional Portfolio</h1>
+        <div class="row">
+          <div class="d-inline-flex p-2 bd-highlight">
+            <ul>
+              <li id="reach">Reach Out To Me:</li>
+              <li>Cell: 267-671-4412</li>
+              <li>Zmathuthu@yahoo.com</li>
+            </ul>
+          </div>
+          <div class="d-inline-flex p-2 bd-highlight">
+            Professional Background: Zana Mathuthu graduated from Ursinus
+            College n 2012 with a BA degree in Ecomonics, with minors in
+            Mathematics and Statistics. She has 10 years of experience in B2B
+            sales in Technology Services and Staffing, and 4 years of management
+            experience. In 2019, Zana began helping small businesses grow their
+            online presence using digital marketing and decided to grow her
+            skills as a developer, in order to assist companies with more
+            complex projects. As Zana continues to grow her skillset, she is
+            eager to begin a career in full-stack technology.
+          </div>
+        </div>
+      </div>
+    )
+}
